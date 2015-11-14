4fceda8d94150dbc9193096f4b7129e8375bbb25
Adrian Sandu <adrian.sandu@pronetis.com>
2015-11-14 17:50:37 +0200

    Added random level generator demo

---
+++ Assets/Scripts/Demo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Demo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int[,] level = LevelGenerator.Generate (6);
+		for (int i = 0; i < level.GetLength(1); i++)
+		{
+			Debug.Log (level [0, i] + ", " + level [1, i] + ", " + level [2, i] + ", " + level [3, i]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class LevelGenerator
+{
+	// Use this for initialization
+	public static int[,] Generate(int n)
+	{
+		int[,] level = new int[4, n];
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				level [i, j] = Random.Range (0, 4);
+			}
+		}
+		return level;
+	}
+
+}
