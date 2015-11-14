ca11d8e72838f97884158416fee94f5168901365
Ionut Iftimia <ionut.iftimia@pronetis.com>
2015-11-14 21:25:40 +0200

    no message

--- Assets/Scripts/BlockController.cs
+++ Assets/Scripts/BlockController.cs
@@ -4,7 +4,10 @@ using DG.Tweening;
 
 public class BlockController : MonoBehaviour {
 
-	public GameObject cube;
+	public bool isDummy = false;
+
+	public GameObject tumblePivot1;
+	public GameObject tumblePivot2;
 	public Renderer top;
 
 	public int type = 0;
@@ -14,26 +17,147 @@ public class BlockController : MonoBehaviour {
 
 	public Material materialMovable;
 	public Material materialNonMovable;
+	public Material materialMoving;
+
+	public bool canTumble = false;
+	public bool isTumbling = false;
+
+	float tumbleTime = 1;
 
 	public void Init(){
 		if (type == 0)
 		{
-			collider.enabled = false;
+			canTumble = false;
 			top.sharedMaterial = materialNonMovable;
 		}
 		else
 		{
+			canTumble = true;
 			top.sharedMaterial = materialMovable;
 
 			transform.localEulerAngles = Vector3.up * (type - 1) * 90;
 		}
 	}
 
-	public void Tumble(){
-		cube.transform.DOLocalRotate(Vector3.right * 90, 0.3f);
+	public void SetToMatrixPosition(){
+		tumblePivot1.transform.localEulerAngles = Vector3.zero;
+		tumblePivot2.transform.localEulerAngles = Vector3.zero;
+
+		transform.position = new Vector3((column - 1.5f) * 2, 0, row * 2);
 	}
 
+	public bool CanTumble(){
+		if (!canTumble)
+			return false;
+		if (GetNextBlock() == null)
+			return false;
+		return true;
+	}
+
+	public void Tumble1(){
+
+		isTumbling = true;
+
+		if (!isDummy)
+			GameController.levelBlocks[column, row] = null;
+
+		Debug.Log("tumble " + " row" + row + "column" + column);
+		collider.enabled = false;
+		tumblePivot1.transform.DOLocalRotate(Vector3.right * 90, tumbleTime).OnComplete(OnTumble1Complete);
+	}
+
+	void OnTumble1Complete(){
+//		tumblePivot2.transform.DOLocalRotate(Vector3.right * 90, 0.3f).OnComplete(OnTumble2Complete).SetDelay(0.3f);
+		if (isDummy){
+			tumblePivot2.transform.DOLocalRotate(Vector3.right * 90, tumbleTime).OnComplete(OnTumble2Complete).SetDelay(0.3f);
+			return;
+		}
+
+		BlockController nextBlock = GetNextBlock();
+		if (nextBlock != null){
+			if (nextBlock.CanTumble()){
+				tumblePivot2.transform.DOLocalRotate(Vector3.right * 90, tumbleTime).OnComplete(OnTumble2Complete).SetDelay(0.3f);
+				nextBlock.Tumble1();
+			}
+		}
+	}
+
+	void OnTumble2Complete(){
+		isTumbling = false;
+		if (isDummy)
+			return;
+
+		collider.enabled = true;
+
+		int nextColumn;
+		int nextRow;
+		GetNextIdx(out nextColumn, out nextRow);
+
+		if (nextRow != -1){
+			column = nextColumn;
+			row = nextRow;
+			GameController.levelBlocks[column, row] = this;
+		}
+
+		SetToMatrixPosition();
+
+
+	}
+	
 	public void Rotate(){
+		if (isDummy)
+			return;
+		if (isTumbling)
+			return;
+		if (!CanTumble())
+			return;
+
+		Debug.Log("rotate " + " row" + row + "column" + column);
 		transform.Rotate(Vector3.up * 90);
+	}
+
+	public void GetNextIdx(out int nextColumn, out int nextRow){
+		nextColumn = -1;
+		nextRow = -1;
+		if (Mathf.Abs(transform.eulerAngles.y - 0) < 1){
+			//			Debug.Log("up");
+			nextColumn = column;
+			nextRow = row + 1;
+		}
+		else if (Mathf.Abs(transform.eulerAngles.y - 90) < 1){
+			//			Debug.Log("right");
+			nextColumn = column + 1;
+			nextRow = row;
+		}
+		else if (Mathf.Abs(transform.eulerAngles.y - 180) < 1){
+			//			Debug.Log("down");
+			nextColumn = column;
+			nextRow = row - 1;
+		}
+		else if (Mathf.Abs(transform.eulerAngles.y - 270) < 1){
+			//			Debug.Log("left");
+			nextColumn = column - 1;
+			nextRow = row;
+		}
+		
+		if (nextColumn < 0)
+			nextColumn = GameController.columns - 1;
+		if (nextColumn == GameController.columns)
+			nextColumn = 0;
+		if (nextRow < 0 || nextRow >= GameController.rows)
+			nextRow = -1;
+
+	}
+
+	public BlockController GetNextBlock(){
+//		Debug.Log(transform.eulerAngles.y);
+		int nextColumn;
+		int nextRow;
+		GetNextIdx(out nextColumn, out nextRow);
+		if (nextRow == -1)
+			return null;
+
+		return GameController.levelBlocks[nextColumn, nextRow];
+
 	}
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+	public static Camera theCamera;
+
 	public Camera gameCamera;
 
 	public GameObject prefabBlock;
 
 	public GameObject levelParent;
+
+
+	public static int rows = 6;
+	public static int columns = 4;
+
 	// Use this for initialization
 	void Start () {
+		theCamera = gameCamera;
 		InitLevel();
 	}
 	
@@ -24,8 +32,10 @@ public class GameController : MonoBehaviour {
 		if (Input.GetMouseButtonUp(1))
 		{
 			BlockController block = GetBlockForScreenPos(Input.mousePosition);
-			if (block != null)
-				block.Tumble();
+			if (block != null){
+				levelBlocks[block.column, block.row] = null;
+				block.Tumble1();
+			}
 		}
 	}
 
@@ -44,32 +54,47 @@ public class GameController : MonoBehaviour {
 		return null;
 	}
 
+	public static BlockController[,] levelBlocks;
+	public static BlockController dummyBlock;
+
 	void InitLevel()
 	{
-		int rows = 6;
+		GameObject blockGODummy = Instantiate(prefabBlock) as GameObject;
+		dummyBlock = blockGODummy.GetComponent<BlockController>();
+
+		dummyBlock.row = -1;
+		dummyBlock.column = -1;
+
+		dummyBlock.Init();
+	
 
 		int[,] level = LevelGenerator.Generate (rows);
+		levelBlocks = new BlockController[columns, rows];
 
 		for (int row = 0; row < rows; row++)
 		{
-			for (int column = 0; column < 4; column++)
+			for (int column = 0; column < columns; column++)
 			{
 				GameObject blockGO = Instantiate(prefabBlock) as GameObject;
 				BlockController block = blockGO.GetComponent<BlockController>();
+
+				levelBlocks[column, row] = block;
 
 				block.row = row;
 				block.column = column;
 				block.type = level [column, row];
 				block.Init();
+				block.SetToMatrixPosition();
 
 				blockGO.name = " row" + row + "column" + column + " type" + level [column, row];
-
 				blockGO.transform.parent = levelParent.transform;
-				blockGO.transform.position = new Vector3((column - 1.5f) * 2, 0, row * 2);
+
 			}
 			Debug.Log (level [0, row] + ", " + level [1, row] + ", " + level [2, row] + ", " + level [3, row]);
 		}
 
-		gameCamera.transform.position = new Vector3(0, 15, 0);
+		gameCamera.transform.position = new Vector3(0, 12.5f, 5);
 	}
+
+
 }
